191e802ffbef7ed950424a8e732736a9bd72088f
PramAhmad <pramuditaadit2810@gmail.com>
2024-11-19 10:40:52 +0700

    update setting for pgsql

--- resources/views/salesorder/index.blade.php
+++ resources/views/salesorder/index.blade.php
@@ -11,12 +11,7 @@
         <div class="card">
             <header class="card-header noborder">
                 <div class="justify-end flex gap-3 items-center flex-wrap">
-                    @can('salesorder create')
-                    <a class="btn inline-flex justify-center btn-dark rounded-[25px] items-center !p-2 !px-3" href="{{ route('salesorder.create') }}">
-                        <iconify-icon icon="ic:round-plus" class="text-lg mr-1"></iconify-icon>
-                        {{ __('New Sales Order') }}
-                    </a>
-                    @endcan
+                 
                     <a class="btn inline-flex justify-center btn-dark rounded-[25px] items-center !p-2.5" href="{{ route('salesorder.index') }}">
                         <iconify-icon icon="mdi:refresh" class="text-xl"></iconify-icon>
                     </a>
