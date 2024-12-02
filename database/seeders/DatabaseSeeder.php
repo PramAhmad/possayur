422e96b79992db3fe4f44ce96e3b601edd490666
PramAhmad <pramuditaadit2810@gmail.com>
2024-12-02 16:38:58 +0700

    update rol seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,8 +24,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UsersTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
-        $this->call(RoleHasPermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
+        $this->call(RoleHasPermissionsTableSeeder::class);
         $this->call(CategoryTableSeeder::class);
         $this->call(CustomerGroupTableSeeder::class);
         $this->call(CustomerTableSeeder::class);
